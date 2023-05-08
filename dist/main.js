db39b179c10bac923d70d0c3811e22e40f089cdc
tuana <tuananh45468@gmail.com>
2023-05-08 16:40:14 +0700

    complete desktop version

--- dist/main.js
+++ dist/main.js
@@ -4,8 +4,10 @@ function boldQuestion() {
   questions.forEach((question) => {
     question.addEventListener("click", (event) => {
       event.target.classList.toggle("font-bold");
+      event.target.classList.toggle("text-very-dark-desaturated-blue");
     });
   });
 }
-
-boldQuestion();
+document.addEventListener("DOMContentLoaded", () => {
+  boldQuestion();
+});
